e8571974732ce72db5373269f955625dad47b9cc
Lucas Lins <lucas11lins@gmail.com>
2020-10-17 12:21:47 -0300

    Adicionado menu de engenheiro

--- programa.c
+++ programa.c
@@ -11,6 +11,53 @@ char username[16];
 char password[16];
 
 
+void engenheiro(){
+    int selection;
+
+    do{
+        printf(BColorCyan "\n###############################" ResetColor);
+        printf(BColorCyan "\n# " BColorWhite "Engenheiro - %s" BColorCyan "      #" ResetColor, username);
+        printf(BColorCyan "\n# " ResetColor "1 - Iniciar Obra" BColorCyan "            #" ResetColor);
+        printf(BColorCyan "\n# " ResetColor "2 - Contratar Funcionários" BColorCyan "  #" ResetColor);
+        printf(BColorCyan "\n# " ResetColor "3 - Verificar custo da obra" BColorCyan " #" ResetColor);
+        printf(BColorCyan "\n# " ResetColor "4 - Selecionar fornecedor e" BColorCyan " #" ResetColor);
+        printf(BColorCyan "\n# " ResetColor "    finalizar compra" BColorCyan "        #" ResetColor);
+        printf(BColorCyan "\n# " ResetColor "5 - Adicionar mensagem de" BColorCyan "   #" ResetColor);
+        printf(BColorCyan "\n# " ResetColor "    acompanhamento da obra" BColorCyan "  #" ResetColor);
+        printf(BColorCyan "\n# " ResetColor "6 - Desconectar da conta" BColorCyan "    #" ResetColor);
+        printf(BColorCyan "\n###############################" ResetColor);
+        printf("\nO que deseja fazer?");
+        printf(ColorGreen "\n-> " ResetColor);
+        scanf("%d%*c", &selection);
+
+        switch(selection){
+            case 1:
+                break;
+
+            case 2:
+                break;
+
+            case 3:
+                break;
+
+            case 4:
+                break;
+            
+            case 5:
+                break;
+            
+            case 6:
+                printf(ColorYellow "Desconectado, até mais!\n" ResetColor);
+                break;
+            
+            default:
+                printf(ColorRed "Opção desconhecida, tente novamente.\n");
+                engenheiro();   
+                break;
+        }
+    }while(selection =! 6);
+}
+
 void gestor(){
     int selection;
 
@@ -72,6 +119,7 @@ void authlogin(){
     }
     else if(strcmp(passeng,password) == 0){
         loggedin = 1;
+        engenheiro();
     }
     else if(strcmp(passmdo,password) == 0){
         loggedin = 1;
